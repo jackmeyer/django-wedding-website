d409b1f4ffdff8af864181177823b225a094241a
Jack Meyer <jackmeyer@utexas.edu>
2025-12-29 17:56:55 -0500

    second batch

--- bigday/settings.py
+++ bigday/settings.py
@@ -154,13 +154,13 @@ STATICFILES_DIRS = (
 
 # Some default values. Will be overwritten by a localsetting.py (rename 'localsettings.py.template' to 'localsettings.py')
 # This is used in a few places where the names of the couple are used
-BRIDE_AND_GROOM = 'Bride and Groom'
+BRIDE_AND_GROOM = 'Nic and Jack'
 # the date of your wedding
-WEDDING_DATE = 'January 1st, 1969'
+WEDDING_DATE = 'June 20th, 2026'
 # the location of your wedding
-WEDDING_LOCATION = 'North Pole, USA'
+WEDDING_LOCATION = 'Victoria, BC'
 # This is used in links in save the date / invitations
-WEDDING_WEBSITE_URL = 'https://thehappycouple.com'
+WEDDING_WEBSITE_URL = 'https://nicandjack.wedding'
 # base address for all emails
 DEFAULT_WEDDING_EMAIL = 'happilyeverafter@example.com'
 WEDDING_CC_LIST = [] 
