8d26a1182816b06741e769658155e4cf689cebea
Jack Meyer <meyer.jackc@gmail.com>
2026-01-05 07:13:10 -0800

    landing page improvements

--- bigday/static/creative/js/creative.js
+++ bigday/static/creative/js/creative.js
@@ -37,6 +37,35 @@
         }
     );
 
+    // Show navbar on scroll down
+    var navbar = $('#mainNav');
+    var navbarHeight = navbar.height();
+    var lastScrollTop = 0;
+    
+    // Initially hide the navbar
+    navbar.hide();
+    
+    $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        
+        // If we've scrolled more than navbar height, show the navbar
+        if (scrollTop > navbarHeight) {
+            navbar.show();
+        }
+        
+        // If scrolling up, show navbar
+        if (scrollTop < lastScrollTop && scrollTop > 0) {
+            navbar.show();
+        }
+        
+        // If at top of page, hide navbar
+        if (scrollTop === 0) {
+            navbar.hide();
+        }
+        
+        lastScrollTop = scrollTop;
+    });
+
     // Offset for Main Navigation - this turns the nav bar white
     $('#mainNav').affix({
         offset: {
